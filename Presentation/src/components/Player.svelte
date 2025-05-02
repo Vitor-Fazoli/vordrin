d359d0055130349d79f8c406302f7daa7f36473c
Vitor-Fazoli <vhfazoli@gmail.com>
2025-05-02 17:22:12 -0300

    feature: working on initial screen and new assets

---
+++ Presentation/src/components/Player.svelte
@@ -0,0 +1,7 @@
+<script>
+	import Health from './Health.svelte';
+</script>
+
+<div class="border-primary border">
+	<Health health={100} maxHealth= {100} widthBar={100} />
+</div>
--- Presentation/src/routes/+page.svelte
+++ Presentation/src/routes/+page.svelte
@@ -1,17 +1,58 @@
 <script>
 	import { goto } from '$app/navigation';
-	import { _ } from 'svelte-i18n';
+	import Mushroom from '$lib/assets/mushroom_login.png';
+
+	let email;
+	let username;
+	let password;
+
+	function handleRegister() {
+		// Prevent the default form submission
+		event.preventDefault();
+
+		// Perform registration logic here (e.g., API call)
+		console.log('Registering:', { email, username, password });
+
+		// Redirect to the login page after successful registration
+		window.location.href = '/login';
+	}
 </script>
 
-<div class="flex h-screen w-screen flex-col items-center justify-center bg-black text-white">
-	<main class="flex flex-col items-center justify-center gap-5 p-2">
-		<h1 class=" text-9xl font-black text-rose-700 underline">VORDRIN</h1>
-		<div class="flex gap-5">
-			<button class="cursor-pointer hover:underline" on:click={goto('/login')}>{$_('login')}</button
-			>
-			<button class="cursor-pointer hover:underline" on:click={goto('/register')}
-				>{$_('register')}</button
+<div class="flex h-screen w-screen flex-col items-center justify-center gap-5 bg-black text-white">
+	<div class="flex flex-row items-center justify-center gap-[100px]">
+		<img src={Mushroom} alt="" />
+		<div>
+			<form class="m-0 flex flex-col gap-2 p-0">
+				<h1 class="text-primary text-5xl font-black underline">Vordrin</h1>
+				<p class="text-xs">Username</p>
+				<input
+					class="border-primary border pl-2"
+					type="text"
+					name="username"
+					required
+					bind:value={username}
+				/>
+				<p class="text-xs">Password</p>
+				<input
+					class="border-primary border pl-2"
+					type="password"
+					name="password"
+					required
+					bind:value={password}
+				/>
+				<button
+					class="text-primary border-primary cursor-pointer border duration-150 hover:border-white hover:bg-white hover:text-black"
+					type="submit">Sign-in</button
+				>
+			</form>
+			<button
+				onclick={goto('/register')}
+				class="hover:bg-primary hover:none w-full cursor-pointer text-sm underline duration-150"
+				>Create your account</button
 			>
 		</div>
-	</main>
+	</div>
+	<a href="https://github.com/Vitor-Fazoli" class="cursor-pointer text-xs text-white underline"
+		>Developed by Vitor Fazoli</a
+	>
 </div>
